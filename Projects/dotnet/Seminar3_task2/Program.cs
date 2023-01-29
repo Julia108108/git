4da165547ea20a986bca54ae3e9819018fac0c66
Julia <matterfixed@gmail.com>
2023-01-29 16:01:37 +0300

    task2

---
+++ Projects/dotnet/Seminar3_task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X не ранво 0 и Y не равно 0, и выдает номер четверти плоскости,
+// в которой находится эта точка.
+// Оси:
+// 2  1
+// 3  4
+
+System.Console.WriteLine ("--> ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine ("--> ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) System.Console.WriteLine ("1 quarter");
+else if (x < 0 && y > 0) System.Console.WriteLine ("2 quarter");
+else if (x < 0 && y < 0) System.Console.WriteLine ("3 quarter");
+else if (x > 0 && y < 0) System.Console.WriteLine ("4 quarter");
