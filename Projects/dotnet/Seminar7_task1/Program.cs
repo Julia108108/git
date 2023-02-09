922660a5cdaf443b5bba593fc3fdfa8a9af559c9
Julia <matterfixed@gmail.com>
2023-02-09 13:22:44 +0300

    done

---
+++ Projects/dotnet/Seminar7_task1/Program.cs
@@ -0,0 +1,34 @@
+﻿int InputIntNumber(string numberName)
+{
+    System.Console.Write($"Input {numberName} :");
+   int intNumber = Convert.ToInt32(Console.ReadLine());
+    return intNumber;}
+
+void FillArray2D(int [,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j]=i+j;
+    }
+}
+}
+
+void PrintIntArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int  m =InputIntNumber("число m");
+int  n =InputIntNumber("число n");
+int [,] array2D= new int [m,n];
+FillArray2D(array2D);
+PrintIntArray2D(array2D);
