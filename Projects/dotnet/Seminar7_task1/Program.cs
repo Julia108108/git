d96f8304751f067b97dfd9d2ed8e32cf1921c1d2
Julia <matterfixed@gmail.com>
2023-02-09 13:23:07 +0300

    done

--- Projects/dotnet/Seminar7_task1/Program.cs
+++ Projects/dotnet/Seminar7_task1/Program.cs
@@ -1,4 +1,12 @@
-﻿int InputIntNumber(string numberName)
+﻿// Задача 48. Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле:
+// Amn = m + n. Выведите полученный массив на экран
+// m = 3, n = 4
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int InputIntNumber(string numberName)
 {
     System.Console.Write($"Input {numberName} :");
    int intNumber = Convert.ToInt32(Console.ReadLine());
@@ -32,3 +40,24 @@ int  n =InputIntNumber("число n");
 int [,] array2D= new int [m,n];
 FillArray2D(array2D);
 PrintIntArray2D(array2D);
+
+//Еще одно решение:
+
+Console.WriteLine("Введите количестов строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количестов столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = new int [m, n] //создали размерность массива
+
+for (int i = 0; i < array.GetLength(0); i++) //i -строка, j - столбец
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array [i, j] = i + j;
+        Console.Write(array [i, j]);
+    }
+    System.Console.WriteLine();
+}
+
